37fa16f5c1040166d1d991d58bda5107fba6ba0c
lvxn0va <lvx.n0va@gmail.com>
2013-02-16 15:38:37 -0800

    Change route to share application

--- lib/catarse_stripe/engine.rb
+++ lib/catarse_stripe/engine.rb
@@ -7,13 +7,13 @@ module ActionDispatch::Routing
   class Mapper
     def mount_catarse_stripe_at(catarse_stripe)
       namespace :payment do
-        get '/stripe/:id/review' => 'stripe#review', :as => 'review_stripe'
-        post '/stripe/notifications' => 'stripe#ipn',  :as => 'ipn_stripe'
-        match '/stripe/:id/notifications' => 'stripe#notifications',  :as => 'notifications_stripe'
-        match '/stripe/:id/pay'           => 'stripe#pay',            :as => 'pay_stripe'
-        match '/stripe/:id/success'       => 'stripe#success',        :as => 'success_stripe'
-        match '/stripe/:id/cancel'        => 'stripe#cancel',         :as => 'cancel_stripe'
-        match '/stripe/:id/charge'        => 'stripe#charge',         :as => 'charge_stripe'
+        get '/stripe/:id/review' => 'catarse_stripe/payment/stripe#review', :as => 'review_stripe'
+        post '/stripe/notifications' => 'catarse_stripe/payment/stripe#ipn',  :as => 'ipn_stripe'
+        match '/stripe/:id/notifications' => 'catarse_stripe/payment/stripe#notifications',  :as => 'notifications_stripe'
+        match '/stripe/:id/pay'           => 'catarse_stripe/payment/stripe#pay',            :as => 'pay_stripe'
+        match '/stripe/:id/success'       => 'catarse_stripe/payment/stripe#success',        :as => 'success_stripe'
+        match '/stripe/:id/cancel'        => 'catarse_stripe/payment/stripe#cancel',         :as => 'cancel_stripe'
+        match '/stripe/:id/charge'        => 'catarse_stripe/payment/stripe#charge',         :as => 'charge_stripe'
       end
     end
   end

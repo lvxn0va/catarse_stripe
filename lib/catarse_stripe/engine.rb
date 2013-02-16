fbf3668ccfc4cf974b3d2b6d676203331c3d5e55
lvxn0va <lvx.n0va@gmail.com>
2013-02-16 11:09:50 -0800

    Change route to share application

--- lib/catarse_stripe/engine.rb
+++ lib/catarse_stripe/engine.rb
@@ -5,8 +5,8 @@
 #end
 module ActionDispatch::Routing
   class Mapper
-    def mount_catarse_stripe_at(payment)
-      scope payment do
+    def mount_catarse_stripe_at(catarse_stripe)
+      namespace :payment do
           get '/stripe/:id/review' => 'stripe#review', :as => 'review_stripe'
           post '/stripe/notifications' => 'stripe#ipn',  :as => 'ipn_stripe'
           match '/stripe/:id/notifications' => 'stripe#notifications',  :as => 'notifications_stripe'

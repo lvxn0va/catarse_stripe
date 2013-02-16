aec9794c9578fe95dd0b6d4edf99fe0a71e05367
lvxn0va <lvx.n0va@gmail.com>
2013-02-16 10:15:27 -0800

    Change route to share application

--- lib/catarse_stripe/engine.rb
+++ lib/catarse_stripe/engine.rb
@@ -1,5 +1,22 @@
-module CatarseStripe
-  class Engine < ::Rails::Engine
-    isolate_namespace CatarseStripe
+#module CatarseStripe
+  #class Engine < ::Rails::Engine
+    #isolate_namespace CatarseStripe
+  #end
+#end
+module ActionDispatch::Routing
+  class Mapper
+    def mount_catarse_stripe_at(mount_location)
+      scope mount_location do
+        namespace :payment do
+          get '/stripe/:id/review' => 'stripe#review', :as => 'review_stripe'
+          post '/stripe/notifications' => 'stripe#ipn',  :as => 'ipn_stripe'
+          match '/stripe/:id/notifications' => 'stripe#notifications',  :as => 'notifications_stripe'
+          match '/stripe/:id/pay'           => 'stripe#pay',            :as => 'pay_stripe'
+          match '/stripe/:id/success'       => 'stripe#success',        :as => 'success_stripe'
+          match '/stripe/:id/cancel'        => 'stripe#cancel',         :as => 'cancel_stripe'
+          match '/stripe/:id/charge'        => 'stripe#charge',         :as => 'charge_stripe'
+        end
+      end
+    end
   end
 end

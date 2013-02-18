3e4df4cd6bb9a94c1aaa39fa2769599747a2b2f0
lvxn0va <lvx.n0va@gmail.com>
2013-02-17 17:12:51 -0800

    fixing connect omniauth routing

--- lib/catarse_stripe/engine.rb
+++ lib/catarse_stripe/engine.rb
@@ -4,5 +4,11 @@ require "omniauth-stripe-connect"
 module CatarseStripe
   class Engine < ::Rails::Engine
     isolate_namespace CatarseStripe
+
+    config.to_prepare do
+      ApplicationController.helper(MyEngineHelper)
+    end
   end
+
+
 end

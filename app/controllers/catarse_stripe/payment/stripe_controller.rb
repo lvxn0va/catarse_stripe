47926f2ce489562914734204b8d19143dd443847
lvxn0va <lvx.n0va@gmail.com>
2013-02-19 15:41:54 -0800

    oops variable

--- app/controllers/catarse_stripe/payment/stripe_controller.rb
+++ app/controllers/catarse_stripe/payment/stripe_controller.rb
@@ -84,7 +84,7 @@ module CatarseStripe::Payment
       access_token = @backer.project.stripe_access_token #Project Owner SECRET KEY
       begin
         customer = Stripe::Customer.create(
-           email: backer.payer_email,
+           email: @backer.payer_email,
            card: params[:stripeToken]
         )
         

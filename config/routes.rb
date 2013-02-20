7ddd17bf1e9ebc2422f5c54ca0d70d935c5a83cf
lvxn0va <lvx.n0va@gmail.com>
2013-02-20 11:04:36 -0800

    change callback route method

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ CatarseStripe::Engine.routes.draw do
   namespace :payment do
     get '/stripe/:id/review' => 'stripe#review', :as => 'review_stripe'
     post '/stripe/notifications' => 'stripe#ipn',  :as => 'ipn_stripe'
+    post '/stripe/callback'          => 'stripe#callback',       :as => 'stripe_auth_callback'
     match '/stripe/:id/notifications' => 'stripe#notifications',  :as => 'notifications_stripe'
     match '/stripe/:id/pay'           => 'stripe#pay',            :as => 'pay_stripe'
     match '/stripe/:id/success'       => 'stripe#success',        :as => 'success_stripe'
     match '/stripe/:id/cancel'        => 'stripe#cancel',         :as => 'cancel_stripe'
     match '/stripe/:id/charge'        => 'stripe#charge',         :as => 'charge_stripe'
     match '/stripe/auth'              => 'stripe#auth',           :as => 'stripe_auth'
-    match '/stripe/callback'          => 'stripe#callback',       :as => 'stripe_auth_callback'
   end
 end

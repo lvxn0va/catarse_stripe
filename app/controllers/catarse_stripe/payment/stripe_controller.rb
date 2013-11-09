4354401dc9ba88a4b755a218e8cf776f4e639e25
Alexandre Daoud <alex.daoud@mac.com>
2013-11-09 23:37:12 +0000

    use catarse_fee config option for application fee

--- app/controllers/catarse_stripe/payment/stripe_controller.rb
+++ app/controllers/catarse_stripe/payment/stripe_controller.rb
@@ -118,7 +118,7 @@ module CatarseStripe::Payment
           amount: @backer.price_in_cents,
           currency: 'usd',
           description: t('stripe_description', scope: SCOPE, :project_name => @backer.project.name, :value => @backer.display_value),
-          application_fee: @backer.platform_fee.to_i
+          application_fee: ::Configuration['catarse_fee']
           },
           access_token #ACCESS_TOKEN (Stripe Secret Key of Connected Project Owner NOT platform)
         )

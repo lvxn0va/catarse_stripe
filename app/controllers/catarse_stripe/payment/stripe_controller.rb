dc645b6597fb6daeb18dfbb097a05f1d1f280eca
Alexandre Daoud <alex.daoud@mac.com>
2013-11-10 00:04:49 +0000

    now convert it all to an integer...

--- app/controllers/catarse_stripe/payment/stripe_controller.rb
+++ app/controllers/catarse_stripe/payment/stripe_controller.rb
@@ -118,7 +118,7 @@ module CatarseStripe::Payment
           amount: @backer.price_in_cents,
           currency: 'usd',
           description: t('stripe_description', scope: SCOPE, :project_name => @backer.project.name, :value => @backer.display_value),
-          application_fee: @backer.price_in_cents * ::Configuration['catarse_fee'].to_f
+          application_fee: (@backer.price_in_cents * ::Configuration['catarse_fee'].to_f).to_i
           },
           access_token #ACCESS_TOKEN (Stripe Secret Key of Connected Project Owner NOT platform)
         )
